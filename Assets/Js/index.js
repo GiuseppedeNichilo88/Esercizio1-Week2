3aba75c0ec2c3c686b4f88bdae4e42df072da93d
Pinucc <123814417+Pinucc@users.noreply.github.com>
2023-02-06 15:57:37 +0100

    Add files via upload

---
+++ Assets/Js/index.js
@@ -0,0 +1,11 @@
+document.write('<h1>Iniziamo la nostra pratica</h1>');
+
+document.write('<h3>Outputs scritto da Javascript</h3>');
+
+document.write('<h3>pantera scritto da Javascript</h3>');
+
+
+window.alert('Pagina non trovata!');
+alert('Riprova')
+
+
---
+++ Assets/Js/variabili.js
@@ -0,0 +1,12 @@
+
+
+var criceto = 'cricetoBianco'
+console.log('cricetoBianco');
+
+
+var leone_scuro = 'leone_scuro'
+console.log('leone_scuro')
+
+
+var GiraffaColloLungo = 'GiraffaColloLungo'
+console.log('GiraffaColloLungo')
